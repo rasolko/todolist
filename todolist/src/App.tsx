50be056820d20144826c67544860e8455f037593
rasolko <ivan.rasolko1@gmail.com>
2022-03-13 11:49:01 +0300

    feat: add todolists state and tasks state, App return Todolist.tsx with map

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {v1} from 'uuid';
 import './App.css';
+import {Todolist} from './Todolist';
 
+export type TodolistsObjectType = {
+  id: string
+  title: string
+  filter: string
+}
 function App() {
-  return (
-    <div className="App">
+    let todolistId1 = v1();
+    let todolistId2 = v1();
+    let [todolists, setTodolists] = useState<Array<TodolistsObjectType>>([
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'}
+    ]);
+    let [tasks, setTasks] = useState({
+        [todolistId1]: [
+            {id: v1(), title: 'HTML&CSS', isDone: true},
+            {id: v1(), title: 'JS', isDone: true}
+        ],
+        [todolistId2]: [
+            {id: v1(), title: 'Milk', isDone: true},
+            {id: v1(), title: 'Bread', isDone: true}
+        ]
+    });
 
-    </div>
-  );
+
+    return (
+        <div className="App">
+          {todolists.map( el => {
+            return <Todolist filter={el.filter} title={el.title}/>
+          })}
+        </div>
+    );
 }
 
 export default App;
--- todolist/src/Todolist.tsx
+++ todolist/src/Todolist.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import {TodolistsObjectType} from './App';
 
-export const Todolist = () => {
+type TodolistPropsType = {
+    title: string
+    filter: string
+}
+export const Todolist: React.FC<TodolistPropsType> = (props) => {
     return (
         <div>
-            
+            <h3>
+                {props.title}
+                <button>x</button>
+            </h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li>
+                    <input type="checkbox"/>
+                    <span>task 1</span>
+                    <button>x</button>
+                </li>
+                <li>
+                    <input type="checkbox"/>
+                    <span>task 2</span>
+                    <button>x</button>
+                </li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
         </div>
     );
 };
